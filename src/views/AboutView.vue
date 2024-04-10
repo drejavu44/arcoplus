cfcbca563f7a1667e5f526d63e73f2f7a781a1b2
Andrei <johnandrei.riego.cics@ust.edu.ph>
2024-04-10 15:25:46 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/main.js 	new file:   src/router/index.js 	new file:   src/views/AboutView.vue 	new file:   src/views/HomeView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+    <Navbar />
+Hi this is the about page
+ulul
+<Footer />
+</template>
+
+<script setup>
+  import Navbar from "../components/navbar.vue";
+  import Footer from "../components/footer.vue";
+</script>
