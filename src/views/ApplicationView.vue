84189a6b3e119af95b1278c4cdf1638ca2b658fd
Andrei <johnandrei.riego.cics@ust.edu.ph>
2024-04-23 16:42:32 +0800

    renamed:    src/components/navbar-about.vue -> src/components/navbar-application.vue 	modified:   src/components/navbar.vue 	modified:   src/components/quoteform.vue 	deleted:    src/components/socials.vue 	modified:   src/router/index.js 	deleted:    src/views/AboutView.vue 	new file:   src/views/ApplicationView.vue 	modified:   src/views/HomeView.vue

---
+++ src/views/ApplicationView.vue
@@ -0,0 +1,308 @@
+<template>
+  <div class="about-container">
+    <div class="about-background-div">
+      <Navbar />
+      <div class="opening-text">
+        <h1>Work With Us</h1>
+        <p>Join our dynamic team and be part of something extraordinary.</p>
+      </div>
+    </div>
+  </div>
+  <!--Work With Us-->
+  <div class="wwu-container">
+    <div class="wwu-section-header">
+      <h1>Application Form</h1>
+      <p>Fill in the necessary information to submit your application. We will get back to you once it has been checked.</p>
+    </div>
+    <div class="wwu">
+      <div class="wwu-3">
+        <div class="wwu-4">
+          <div class="wwu-column-2">
+            <div class="wwu-5">
+              <div class="wwu-7">
+                <form>
+                  <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required />
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                  </div>
+                  <div class="form-group">
+                    <label for="phone">Phone:</label>
+                    <input type="tel" id="phone" name="phone" required />
+                  </div>
+                  <div class="form-group">
+                    <label for="jobPosition">Job Position:</label>
+                    <select id="jobPosition" name="jobPosition" required>
+                      <option value="" disabled selected>
+                        Select a job position
+                      </option>
+                      <option value="1">Point Guard</option>
+                      <option value="2">Shooting Guard</option>
+                      <option value="3">Security Guard</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label for="phone">Resume:</label>
+                    <input type="file" id="resume" name="resume" required />
+                  </div>
+                  <button type="submit" @click="handleButtonClick">
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Footer />
+</template>
+
+<script setup>
+import Navbar from "../components/navbar-application.vue";
+import Footer from "../components/footer.vue";
+import { ref } from "vue";
+import Swal from "sweetalert2";
+
+const formData = ref({
+  name: "",
+  email: "",
+  phone: "",
+  jobposition: "",
+  resume: "",
+});
+
+const handleButtonClick = () => {
+  // Handle form submission logic here
+
+  Swal.fire({
+    icon: "success",
+    title: "Application received!",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+};
+</script>
+
+<style scoped>
+.about-container {
+  margin: 0;
+}
+
+.about-background-div {
+  background-image: url("../assets/ap-bg-3.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.opening-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 800px;
+  font-size: 45px;
+  color: white;
+  font-family: Poppins, sans-serif;
+  text-align: center;
+  animation: fadeInUp 1s ease forwards;
+}
+
+.opening-text .content {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  color: white;
+  font-family: Poppins, sans-serif;
+  margin-top: -45px;
+  opacity: 0;
+  animation: fadeInUp 1s ease forwards;
+}
+
+.opening-text p {
+  font-size: 25px;
+  margin-top: -35px;
+}
+
+
+/*Work With Us*/
+.wwu-container {
+  background-color: rgba(240, 240, 240, 0.45);
+}
+
+.wwu-section-header {
+  padding: 35px;
+  display: flex;
+  flex-direction: column;
+  color: #1e1e1e;
+  font-size: 13px;
+  font-family: Poppins, sans-serif;
+  font-weight: bold;
+  align-items: center;
+}
+
+.wwu-section-header h1 {
+  border-bottom: 8px solid rgba(205, 171, 100, 1);
+}
+
+.wwu-section-header p {
+  color: #8f8f8f;
+  font-size: 15px;
+  font-family: Poppins, sans-serif;
+  font-weight: normal;
+  margin-bottom: -2%;
+}
+
+.wwu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 100%;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  padding: 15px;
+}
+
+.wwu-3 {
+  background-color: white;
+  width: 100%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  overflow: auto;
+}
+
+.wwu-4 {
+  gap: 20px;
+  display: flex;
+}
+
+.wwu-column-2 {
+  display: flex;
+  flex-direction: column;
+  line-height: normal;
+  width: 100%;
+  margin-right: 20px;
+}
+
+.wwu-5 {
+  display: flex;
+  flex-direction: column;
+  align-self: stretch;
+  margin: auto 0;
+}
+
+.wwu-7 {
+  color: #1e1e1e;
+  font-family: Poppins, sans-serif;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 25px;
+  margin-top: -75px;
+  text-align: center;
+}
+
+.wwu-7 h1 {
+  margin-bottom: 40px;
+  border-bottom: 1px solid #e0e0e0;
+  padding: 10px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+form {
+  max-width: 100%;
+  margin-top: 75px;
+}
+
+.form-group label {
+  display: block;
+  margin-bottom: 5px;
+  text-align: left;
+  color: #1e1e1e;
+  font-weight: bold;
+  font-size: 12px;
+}
+
+.form-group select {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-family: Poppins, sans-serif;
+  box-sizing: border-box;
+  -webkit-appearance: none; /* Removes default arrow on Chrome/Safari */
+  -moz-appearance: none; /* Removes default arrow on Firefox */
+  appearance: none; /* Removes default arrow on other browsers */
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='%231e1e1e'%3E%3Cpath d='M6 8l4 4 4-4'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: right 10px top 50%;
+  background-size: 12px auto;
+}
+
+.form-group :focus {
+  border-color: rgba(205, 171, 100, 1); /*Apply to other forms*/
+  outline: none;
+}
+
+input {
+  width: 100%;
+  max-width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing: border-box;
+  font-family: Poppins, sans-serif;
+  font-size: 14px;
+}
+
+button[type="submit"] {
+  background-color: transparent;
+  border-radius: 30px;
+  border-color: rgba(205, 171, 100, 1);
+  border-style: solid;
+  border-width: 1px;
+  color: rgba(205, 171, 100, 1);
+  width: 100%;
+  font-family: Poppins, sans-serif;
+  cursor: pointer;
+  transition: background-color 0.5s ease;
+  margin-top: 20px;
+  padding: 10px 20px;
+}
+
+button[type="submit"]:hover {
+  background-color: rgba(205, 171, 100, 1);
+  color: white;
+}
+
+input[type="file"] {
+  background-color: #f9f9f9;
+}
+
+input[type="file"]::-webkit-file-upload-button {
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: transparent;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home-container">
     <div class="home-background-div">
       <Navbar />
-      <div id ="home" class="opening-text">
+      <div id="home" class="opening-text">
         <h1>Transforming Ideas Into Structures</h1>
         <div class="content">
           <p>Got a project for us? We’ve got you covered.</p>
@@ -10,7 +10,6 @@
         </div>
       </div>
     </div>
-    <!--Socials <Social />-->
     <!-- About Us -->
     <div id="about" class="about-us">
       <div class="about-us-2">
@@ -30,33 +29,94 @@
               type and scrambled it to make a type specimen book.
             </div>
             <a class="about-us-7"
-              ><RouterLink to="/about">LEARN MORE</RouterLink>
+              ><RouterLink to="/application">APPLY NOW!</RouterLink>
             </a>
           </div>
         </div>
       </div>
     </div>
   </div>
-    <!--Project-->
-    <div id ="projects">
+  <!--Why Choose Us-->
+  <div class="wcu-container">
+    <div class="wcu-section-header">
+      <h1>Why Choose Us?</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+    </div>
+    <div class="wcu">
+      <div class="wcu-2">
+        <div class="wcu-column">
+          <div class="wcu-3">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/c34898178e2dea90e403576f1cfdf41526f3610d730034628f0f4618b0acf64d?"
+              class="img"
+            />
+            <div class="wcu-4">Expertise in Diverse Projects</div>
+            <div class="wcu-5">
+              From residential to commercial and industrial ventures, our
+              construction company specializes in a wide array of projects,
+              ensuring tailored solutions to meet every client's needs.
+            </div>
+          </div>
+        </div>
+        <div class="wcu-column-2">
+          <div class="wcu-6">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/e6a7bc941146df329f4d711ab2a33780f864a20ebdc0d65366ed37ce1a397be6?"
+              class="img-2"
+            />
+            <div class="wcu-7">Sustainable Solutions</div>
+            <div class="wcu-8">
+              Committed to environmental responsibility, we integrate
+              sustainable practices into our projects, minimizing our ecological
+              footprint with eco-friendly materials and energy-efficient
+              designs.
+            </div>
+          </div>
+        </div>
+        <div class="wcu-column-3">
+          <div class="wcu-9">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/536492aff729039dc4b210f795440a3e4a312af37504541ca09729ebd58df30a?"
+              class="img-3"
+            />
+            <div class="wcu-10">
+              Experienced <br />
+              Team
+            </div>
+            <div class="wcu-11">
+              Our team comprises skilled architects, engineers, project
+              managers, and laborers, bringing years of collective experience to
+              every project.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Project-->
+  <div id="projects">
     <Project />
   </div>
-    <!--Product-->
-    <div id ="products">
+  <!--Product-->
+  <div id="products">
     <Product />
   </div>
-    <!--Quote Form-->
-    <div id ="quote">
+  <!--Quote Form-->
+  <div id="quote">
     <QuoteForm />
   </div>
-  <!--CRUD-->
   <Footer />
 </template>
 
 <script setup>
 import Navbar from "../components/navbar.vue";
 import Footer from "../components/footer.vue";
-import Social from "../components/socials.vue";
 import Product from "../components/product.vue";
 import Project from "../components/project.vue";
 import QuoteForm from "../components/quoteform.vue";
@@ -150,6 +210,7 @@ const scrollToSection = (sectionId) => {
 
 .about-us {
   padding: 80px;
+  background-color: rgba(240, 240, 240, 0.45);
 }
 
 .about-us-2 {
@@ -238,5 +299,113 @@ const scrollToSection = (sectionId) => {
   color: white;
 }
 
+/*Why Choose Us?*/
 
+.wcu-section-header {
+  padding: 35px;
+  display: flex;
+  flex-direction: column;
+  color: #1e1e1e;
+  font-size: 13px;
+  font-family: Poppins, sans-serif;
+  font-weight: bold;
+  align-items: center;
+}
+
+.wcu-section-header h1 {
+  border-bottom: 8px solid rgba(205, 171, 100, 1);
+}
+
+.wcu-section-header p {
+  color: #8f8f8f;
+  font-size: 15px;
+  font-family: Poppins, sans-serif;
+  font-weight: normal;
+}
+
+.wcu {
+  max-width: 90%;
+  margin: 0 auto;
+  margin-bottom: 50px;
+  padding: 5px;
+}
+
+.wcu-2 {
+  gap: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.wcu-column,
+.wcu-column-2,
+.wcu-column-3 {
+  display: flex;
+  flex-direction: column;
+  line-height: normal;
+  width: 50%;
+  margin-left: 0px;
+  height: 300px; 
+}
+
+.wcu-3,
+.wcu-6,
+.wcu-9 {
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  padding: 56px 11px 34px;
+  max-height: 100%; 
+  overflow: hidden; 
+}
+
+.img,
+.img-2,
+.img-3 {
+  aspect-ratio: 1.04;
+  object-fit: auto;
+  object-position: center;
+  width: 49px;
+  align-self: center;
+}
+
+.wcu-4,
+.wcu-7,
+.wcu-10 {
+  color: #1e1e1e;
+  margin-top: 31px;
+  font: 500 20px/100% Poppins, sans-serif;
+  text-align: center;
+}
+
+.wcu-5,
+.wcu-8,
+.wcu-11 {
+  color: #949494;
+  text-align: center;
+  margin-top: 23px;
+  font: 400 12px/19px Poppins, sans-serif;
+}
+
+.wcu-column .wcu-3,
+.wcu-column-2 .wcu-6,
+.wcu-column-3 .wcu-9 {
+  transition: transform 0.3s ease;
+}
+
+.wcu-column:hover .wcu-3,
+.wcu-column-2:hover .wcu-6,
+.wcu-column-3:hover .wcu-9 {
+  transform: scale(1.1);
+}
+
+@media only screen and (max-width: 768px) {
+  .wcu-5,
+  .wcu-8,
+  .wcu-11 {
+    overflow: auto; 
+  }
+}
 </style>
