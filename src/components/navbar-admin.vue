5673c0d63d3577c19927ac69ce8b53a7826c4129
Andrei <johnandrei.riego.cics@ust.edu.ph>
2024-04-24 20:37:01 +0800

    modified:   src/components/navbar-admin.vue

--- src/components/navbar-admin.vue
+++ src/components/navbar-admin.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="ah-navbar">
-    <RouterLink to="/adminhome" class="nav-item" :class="{ 'active': $route.path === '/adminhome' }" exact>
+    <RouterLink
+      to="/adminhome"
+      class="nav-item"
+      :class="{ active: $route.path === '/adminhome' }"
+      exact
+    >
       <i class="fas fa-home"></i> Overview
     </RouterLink>
     |
-    <RouterLink to="/products" class="nav-item" :class="{ 'active': $route.path === '/products' }" exact>
+    <RouterLink
+      to="/products"
+      class="nav-item"
+      :class="{ active: $route.path === '/products' }"
+      exact
+    >
       <i class="fas fa-cube"></i> Products
     </RouterLink>
     |
-    <RouterLink to="/projects" class="nav-item" :class="{ 'active': $route.path === '/projects' }" exact>
+    <RouterLink
+      to="/projects"
+      class="nav-item"
+      :class="{ active: $route.path === '/projects' }"
+      exact
+    >
       <i class="fas fa-project-diagram"></i> Projects
+    </RouterLink>
+    |
+    <RouterLink
+      to="/login"
+      class="nav-item"
+    >
+      <i class="fas fa-sign-out-alt"></i> Sign Out
     </RouterLink>
   </div>
 </template>
@@ -32,6 +54,10 @@ import { RouterLink } from "vue-router";
   color: #e5e6e8;
   margin: 0 10px;
   transition: color 0.3s ease;
+}
+
+.nav-item:hover {
+  color: rgba(205, 171, 100, 1);
 }
 
 .nav-item.active {
