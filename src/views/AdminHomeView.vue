1ae188984c6b689f1f76c21d928c440b5159a70e
Andrei <johnandrei.riego.cics@ust.edu.ph>
2024-04-25 16:02:22 +0800

    modified:   src/views/AdminHomeView.vue

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -1,17 +1,19 @@
 <script setup>
-import AdminNavbar from "../components/navbar-admin.vue"
-import { ref, onMounted } from "vue"
+import AdminNavbar from "../components/navbar-admin.vue";
+import { ref, onMounted } from "vue";
 import { getUserSession } from "@/supabase/supabase";
 
-const userAccount = ref()
+const activeTab = ref("jobApplications");
+
+const userAccount = ref();
 
 const getUserAccount = async () => {
-  userAccount.value = await getUserSession()
-}
+  userAccount.value = await getUserSession();
+};
 
 onMounted(async () => {
-  await getUserAccount()
-})
+  await getUserAccount();
+});
 </script>
 
 <template>
@@ -19,9 +21,15 @@ onMounted(async () => {
     <div class="adminhome-background-div">
       <div id="home" class="opening-text">
         <AdminNavbar />
-        <h1>Welcome, <b>{{ userAccount?.session.user.user_metadata.firstName }}</b></h1>
+        <h1>
+          Welcome,
+          <b>{{ userAccount?.session.user.user_metadata.firstName }}</b>
+        </h1>
         <div class="content">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
         </div>
       </div>
     </div>
@@ -34,14 +42,89 @@ onMounted(async () => {
           and job applications.
         </p>
       </div>
+      <!-- Tab Navigation -->
+      <div class="jaq-tab-navigation">
+        <button
+          :class="{
+            'jaq-tab-btn': true,
+            active: activeTab === 'jobApplications',
+          }"
+          @click="activeTab = 'jobApplications'"
+        >
+          Job Applications
+        </button>
+        <button
+          :class="{ 'jaq-tab-btn': true, active: activeTab === 'quotations' }"
+          @click="activeTab = 'quotations'"
+        >
+          Quotations
+        </button>
+      </div>
+      <!-- Tab Content -->
+      <div v-if="activeTab === 'jobApplications'" class="ah-container">
+        <table class="jaq-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Job Position</th>
+              <th>Resume</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Job Applications -->
+            <tr>
+              <td>Cardo Dalisay</td>
+              <td>cdcutie@gmail.com</td>
+              <td>143-143-143</td>
+              <td>Installer</td>
+              <td><a href="path_to_resume">Download Resume</a></td>
+              <td>
+                <button class="jaq-button"><i class="fa fa-envelope"></i> Send Email</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else class="ah-container">
+        <table class="jaq-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Message</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Quotations-->
+            <tr>
+              <td>Cardo Dalisay</td>
+              <td>cdcutie@gmail.com</td>
+              <td>143-143-143</td>
+              <td>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </td>
+              <td>
+                <button class="jaq-button"><i class="fa fa-envelope"></i> Send Email</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .adminhome-container {
-  margin: 0;
-  /* Adjust margin */
+  margin: 0; /* Adjust margin */
 }
 
 .adminhome-background-div {
@@ -50,6 +133,7 @@ onMounted(async () => {
   background-position: center;
   background-repeat: no-repeat;
   height: 500px;
+  max-width: 100%; /* Set maximum width */
 }
 
 @keyframes fadeInUp {
@@ -57,7 +141,6 @@ onMounted(async () => {
     opacity: 0;
     transform: translateY(20px);
   }
-
   to {
     opacity: 1;
     transform: translateY(0);
@@ -88,7 +171,6 @@ onMounted(async () => {
   justify-content: center;
   align-items: center;
   margin-top: -75px;
-  ;
   font-size: 22px;
   color: white;
   font-family: Poppins, sans-serif;
@@ -103,7 +185,16 @@ onMounted(async () => {
 .ah-container {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  font-family: Poppins, sans-serif;
+  padding: 20px;
+}
+
+.ah-container h2 /* table title */ {
+  font-size: 16px;
+  letter-spacing: 1.5px;
+  font-weight: normal;
+  text-transform: uppercase;
+  color: #8f8f8f;
 }
 
 .ah-section-header {
@@ -135,5 +226,79 @@ onMounted(async () => {
   align-items: center;
   background-color: rgba(240, 240, 240, 0.45);
   min-width: 100%;
+}
+
+/* Table */
+
+.jaq-table {
+  border-collapse: collapse;
+  max-width: 100%;
+  font-family: Poppins, sans-serif;
+  border-top: 4px solid rgba(205, 171, 100, 1);
+  table-layout: fixed;
+}
+
+.jaq-table th,
+.jaq-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+  overflow-wrap: break-word;
+  max-width: 200px;
+  padding: 10px;
+}
+
+.jaq-table a {
+  text-decoration: none;
+  color: rgba(205, 171, 100, 1);
+}
+
+/* Tabs */
+.jaq-tab-navigation {
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 20px;
+}
+
+.jaq-tab-btn {
+  font-family: Poppins, sans-serif;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  background-color: transparent;
+  transition: color 0.3s;
+  color: #8f8f8f;
+}
+
+.jaq-tab-btn:focus {
+  outline: none;
+}
+
+.jaq-tab-btn:hover {
+  color: rgba(205, 171, 100, 1);
+}
+
+.jaq-tab-btn.active {
+  color: rgba(205, 171, 100, 1);
+}
+
+.jaq-button {
+  background-color: transparent;
+  border-radius: 30px;
+  border-color: rgba(205, 171, 100, 1);
+  border-style: solid;
+  border-width: 1px;
+  color: rgba(205, 171, 100, 1);
+  padding: 5px 20px;
+  font-size: 14px;
+  font-family: Poppins, sans-serif;
+  cursor: pointer;
+  transition: background-color 0.5s ease;
+}
+
+.jaq-button:hover {
+  background-color: rgba(205, 171, 100, 1);
+  color: white;
 }
 </style>
